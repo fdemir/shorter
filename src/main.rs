b70e194c4d9a69ef6f589fe98a51c2cbcd7dafd9
fdemir <frndemir@gmail.com>
2023-02-25 21:58:25 +0300

    feat: enum all ops
    thx @pacak

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,68 @@
-use std::path::PathBuf;
-
 use bpaf::*;
-use url::Url;
+use std::str::FromStr;
+
+#[derive(Debug, Clone)]
+enum Op {
+    Get(String),
+    Save(String),
+}
+
+// TODO: i did not understand why this is needed
+impl FromStr for Op {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parts = s.splitn(2, ' ');
+        let op = parts.next().unwrap();
+        let arg = parts.next().unwrap();
+
+        match op {
+            "get" => Ok(Op::Get(arg.to_string())),
+            "save" => Ok(Op::Save(arg.to_string())),
+            _ => Err(format!("Unknown operation: {}", op)),
+        }
+    }
+}
+
+impl Op {
+    fn handle(&self) {
+        match self {
+            Op::Get(uid) => self.get(uid),
+            Op::Save(url) => self.save(url),
+        }
+    }
+
+    fn get(&self, uid: &str) {
+        todo!("Save {}", uid)
+    }
+
+    fn save(&self, url: &str) {
+        todo!("Save {}", url)
+    }
+}
 
 #[derive(Clone, Debug, Bpaf)]
 #[bpaf(options, version)]
 struct Input {
-    save: Url,
-    get: String,
-    delete: String,
-    list: bool,
-    file: Option<PathBuf>,
+    op: Op,
 }
 
-fn opts() -> OptionParser<Input> {
-    let save = short('s')
-        .long("save")
-        .help("Save a url")
-        .argument::<String>("UID")
-        .parse(|url| Url::parse(&url).map_err(|e| e.to_string()));
-
+fn init_ops() -> OptionParser<Input> {
     let get = short('g')
         .long("get")
-        .help("Get a url")
-        .argument::<String>("UID");
+        .help("Get a shortened URL by UID")
+        .argument::<String>("UID")
+        .map(Op::Get);
 
-    let delete = short('d')
-        .long("delete")
-        .help("Delete a url")
-        .argument::<String>("UID");
+    let op = construct!([get]);
 
-    let list = short('l').long("list").help("List all urls").switch();
-
-    let file = short('f')
-        .long("file")
-        .help("File to save to")
-        .argument::<String>("FILE")
-        .parse(|file| {
-            let path = PathBuf::from(file);
-            if path.exists() {
-                Ok(path)
-            } else {
-                // TODO: print error
-                Err(format!("File does not exist"))
-            }
-        })
-        .optional();
-
-    construct!(Input {
-        save,
-        get,
-        delete,
-        list,
-        file
-    })
-    .to_options()
-    .descr("This is a test program.")
+    construct!(Input { op })
+        .to_options()
+        .descr("Short the given url!")
 }
 
 fn main() {
-    let opts = opts().run();
+    let opts = init_ops().run();
 
-    // TODO: implement...
+    opts.op.handle();
 }
