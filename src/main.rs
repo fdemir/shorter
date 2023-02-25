38eabd279834b7a145273b41c15836d0cc41b8de
Michael Baikov <manpacket@gmail.com>
2023-02-25 14:39:02 -0500

    a bit of changes

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,11 @@
 use bpaf::*;
 use std::str::FromStr;
 
-#[derive(Debug, Clone)]
-enum Op {
-    Get(String),
-    Save(String),
-}
-
-// TODO: i did not understand why this is needed
-impl FromStr for Op {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut parts = s.splitn(2, ' ');
-        let op = parts.next().unwrap();
-        let arg = parts.next().unwrap();
-
-        match op {
-            "get" => Ok(Op::Get(arg.to_string())),
-            "save" => Ok(Op::Save(arg.to_string())),
-            _ => Err(format!("Unknown operation: {}", op)),
-        }
-    }
-}
-
 impl Op {
     fn handle(&self) {
         match self {
-            Op::Get(uid) => self.get(uid),
-            Op::Save(url) => self.save(url),
+            Op::Get { uid } => self.get(uid),
+            Op::Save { url } => self.save(url),
         }
     }
 
@@ -42,27 +19,41 @@ impl Op {
 }
 
 #[derive(Clone, Debug, Bpaf)]
+enum Op {
+    #[bpaf(command)]
+    /// Get something
+    Get {
+        /// help here
+        #[bpaf(positional)]
+        uid: String,
+    },
+    #[bpaf(command)]
+    /// Save something
+    Save {
+        /// help here
+        #[bpaf(positional)]
+        url: String,
+    },
+}
+
+#[derive(Debug, Clone, Bpaf)]
 #[bpaf(options, version)]
+/// description
+///
+///
+/// more description, note empty lines
+///
+///
+/// even more
 struct Input {
+    /// for example
+    verbose: bool,
+    #[bpaf(external)]
     op: Op,
 }
 
-fn init_ops() -> OptionParser<Input> {
-    let get = short('g')
-        .long("get")
-        .help("Get a shortened URL by UID")
-        .argument::<String>("UID")
-        .map(Op::Get);
-
-    let op = construct!([get]);
-
-    construct!(Input { op })
-        .to_options()
-        .descr("Short the given url!")
-}
-
 fn main() {
-    let opts = init_ops().run();
+    let opts = input().run();
 
-    opts.op.handle();
+    todo!("{:?}", opts);
 }
