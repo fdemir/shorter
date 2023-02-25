1bde6ced0e59f84626a034cafa87f3cc569e9a1f
fdemir <frndemir@gmail.com>
2023-02-26 02:31:54 +0300

    feat: impl core features
    refactor later

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,87 @@
+use std::{collections::HashMap, fs::read_to_string};
+
 use bpaf::*;
-use std::str::FromStr;
+
+mod hash;
 
 impl Op {
     fn handle(&self) {
         match self {
             Op::Get { uid } => self.get(uid),
             Op::Save { url } => self.save(url),
+            Op::List => self.list(),
         }
     }
 
     fn get(&self, uid: &str) {
-        todo!("Save {}", uid)
+        let storage_file = "storage.json";
+        let working_dir = std::env::current_dir().unwrap();
+        let storage_path = working_dir.join(storage_file);
+
+        let storage_file_exists = storage_path.exists();
+
+        if storage_file_exists {
+            let file = read_to_string(storage_file).unwrap();
+            let value: HashMap<String, String> =
+                serde_json::from_str(&file).expect("Json was not well formatted!");
+
+            match value.get(uid) {
+                Some(url) => println!("{}", url),
+                None => println!("Not found"),
+            }
+        } else {
+            println!("No storage file found");
+        }
     }
 
     fn save(&self, url: &str) {
-        todo!("Save {}", url)
+        let short_val = hash::gen(url);
+
+        let storage_file = "storage.json";
+        let working_dir = std::env::current_dir().unwrap();
+        let storage_path = working_dir.join(storage_file);
+
+        let storage_file_exists = storage_path.exists();
+
+        if storage_file_exists {
+            let file = read_to_string(storage_file).unwrap();
+            let mut value: HashMap<String, String> =
+                serde_json::from_str(&file).expect("Json was not well formatted!");
+
+            match value.get(&short_val) {
+                Some(_) => println!("Exist: {}", short_val),
+                None => {
+                    value.insert(short_val, url.to_string());
+                    let json = serde_json::to_string(&value).unwrap();
+                    std::fs::write(storage_file, json).unwrap();
+                }
+            }
+        } else {
+            let mut value: HashMap<String, String> = HashMap::new();
+            value.insert(short_val, url.to_string());
+            let json = serde_json::to_string(&value).unwrap();
+            std::fs::write(storage_file, json).unwrap();
+        }
+    }
+
+    fn list(&self) {
+        let storage_file = "storage.json";
+        let working_dir = std::env::current_dir().unwrap();
+        let storage_path = working_dir.join(storage_file);
+
+        let storage_file_exists = storage_path.exists();
+
+        if storage_file_exists {
+            let file = read_to_string(storage_file).unwrap();
+            let value: HashMap<String, String> =
+                serde_json::from_str(&file).expect("Json was not well formatted!");
+
+            for (key, value) in value.iter() {
+                println!("{}: {}", key, value);
+            }
+        } else {
+            println!("No storage file found");
+        }
     }
 }
 
@@ -34,20 +101,16 @@ enum Op {
         #[bpaf(positional)]
         url: String,
     },
+
+    // List all saved items
+    #[bpaf(command)]
+    List,
 }
 
 #[derive(Debug, Clone, Bpaf)]
 #[bpaf(options, version)]
-/// description
-///
-///
-/// more description, note empty lines
-///
-///
-/// even more
+// TODO: Put a description here
 struct Input {
-    /// for example
-    verbose: bool,
     #[bpaf(external)]
     op: Op,
 }
@@ -55,5 +118,5 @@ struct Input {
 fn main() {
     let opts = input().run();
 
-    todo!("{:?}", opts);
+    opts.op.handle();
 }
